9f3222a5cd1e251bc66d8d95efc2bf7f26d16fdf
milescarberry <62004569+milescarberry@users.noreply.github.com>
2021-05-08 14:26:40 +0530

    commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import hashlib
 
 
-class NambiCoin():
+class NambiCoinBlock():
 
 
     # Our Special Constructor Method of our class:
@@ -13,3 +13,56 @@ class NambiCoin():
 
 
         self.latest_transaction_list = latest_transaction_list
+
+
+        self.block_data = "-".join(latest_transaction_list) + "-" + previous_block_hash
+
+
+        self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+
+
+
+        # hexdigest() method returns the hexadecimal code in the form of a string object.
+
+
+
+# Now, let's perform some transactions.
+
+
+t1 = "Aditya sends 2.0 NBC to Ramesh."
+
+t2 = "Ramesh sends 4.1 NBC to Ahilya."
+
+t3 = "Gaurav sends 3.2 NBC to Aditya."
+
+t4 = "Aditya sends 4.22 NBC to Ahilya."
+
+t5 = "Ramesh sends 7.1234 NBC to Gaurav."
+
+t6 = "Mathew sends 9.260 NBC to Aditya."
+
+t7 = "Ravish sends 2.1245 NBC to Ramesh."
+
+
+
+genesis_block = NambiCoinBlock("Genesis Block", [t1, t2])
+
+
+# genesis_block is the "initial" Nambi Coin block.
+
+# genesis_block is an "instance" of the NambiCoinBlock class.
+
+
+print()
+
+
+print(genesis_block.block_data)
+
+
+print()
+
+
+# print(type(genesis_block.block_hash))
+
+
+print(genesis_block.block_hash)
