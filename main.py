196ee81219f172e4a9e06523d671737451119711
milescarberry <62004569+milescarberry@users.noreply.github.com>
2021-05-08 07:59:10 +0530

    commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import hashlib as hash
+import hashlib
+
+
+class NambiCoin():
+
+
+    # Our Special Constructor Method of our class:
+
+    def __init__(self, previous_block_hash, latest_transaction_list):
+
+
+        self.previous_block_hash = previous_block_hash
+
+
+        self.latest_transaction_list = latest_transaction_list
